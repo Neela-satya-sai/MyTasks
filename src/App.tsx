b58932533f64e7d0d6871f54ba885c28589a2d38
neelasatyasai <neelasatyasai9.3@gmail.com>
2025-08-02 23:37:38 +0530

    done upto input taking tasks

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,23 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import { Todo } from './components/Todo';
 
-function App() {
+import './App.css';
+import "./styles.css"
+import InputField from './components/InputField';
+
+const  App : React.FC = () => {
+  
+  const [todo, setTodo] = useState<string>("");
+  const [todos, settodos] = useState<Todo[]>([]);
+
+  
+
+  console.log(todos);
+
   return (
     <div className="App">
-     Tasks
+       <span className='heading'>My Tasks</span>
+       <InputField todo={todo} setTodo={setTodo} todos={todos} handleAdd={settodos}></InputField>
     </div>
   );
 }
---
+++ src/components/InputField.jsx
@@ -0,0 +1,48 @@
+import React from "react";
+import { useRef } from "react";
+
+interface props {
+  todo: String;
+  setTodo: React.Dispatch<React.SetStateAction<string>>;
+  todos: Todo[];
+  handleAdd: React.Dispatch<React.SetStateAction<Todo[]>>;
+}
+
+const InputField: React.FC<props> = ({ todo, setTodo, todos, handleAdd }) => {
+
+  // const inputRef = useRef<HTMLInputElement>(null);
+  const inputRef = useRef<HTMLInputElement>(null);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    if (todo) {
+      handleAdd([...todos, { id: Date.now(), todo: todo, isDone: false }]);
+      setTodo("");
+    }
+  };
+
+  return (
+    <form
+      className="input"
+      onSubmit={(e) => {
+        handleSubmit(e);
+        inputRef.current?.blur();
+      }}
+    >
+      <input
+        type="text"
+        placeholder="Enter task.."
+        className="input__box"
+        value={todo}
+        onChange={(e) => setTodo(e.target.value)}
+        ref={inputRef}
+      ></input>
+      <button type="submit" className="input__submit">
+        go
+      </button>
+    </form>
+  );
+};
+
+export default InputField;
